3f40cb939bde3395d4a5faa8711a4d91467e1e6a
Алина Жирохова <alinazirohova@MacBook-Air-Alina.local>
2022-10-28 15:56:49 +0300

    4.2

--- script.js
+++ script.js
@@ -1,42 +1,6 @@
-// const gameLevelButton1 = document.querySelector('.game__level_button1');
-// const gameLevelButton2 = document.querySelector('.game__level_button2');
-// const gameLevelButton3 = document.querySelector('.game__level_button3');
-// const body = document.querySelector('.body');
-
-// gameLevelButton1.addEventListener('click', getFirstLevelDifficulty);
-// gameLevelButton2.addEventListener('click', getSecondLevelDifficulty);
-// gameLevelButton3.addEventListener('click', getThirdLevelDifficulty);
-
-// function getFirstLevelDifficulty() {
-//     body.textContent = '';
-//     for (let i = 0; i < 6; i++) {
-//         const card = document.createElement('img');
-//         card.src = 'shirt.png';
-
-//         body.appendChild(card);
-//     }
-// }
-
-// function getSecondLevelDifficulty() {
-//     body.textContent = '';
-//     for (let i = 0; i < 12; i++) {
-//         const card = document.createElement('img');
-//         card.src = 'shirt.png';
-
-//         body.appendChild(card);
-//     }
-// }
-
-// function getThirdLevelDifficulty() {
-//     body.textContent = '';
-//     for (let i = 0; i < 18; i++) {
-//         const card = document.createElement('img');
-//         card.src = 'shirt.png';
-
-//         body.appendChild(card);
-//     }
-// }
-
+window.game = {
+    difficultyLevel: getLevelDifficulty
+}
 
 const body = document.querySelector('body');
 const buttonStart = document.querySelector('.game__start');
@@ -44,28 +8,15 @@ const gameLevelButton1 = document.querySelector('.game__level_button1');
 const gameLevelButton2 = document.querySelector('.game__level_button2');
 const gameLevelButton3 = document.querySelector('.game__level_button3');
 
+gameLevelButton1.addEventListener('click', () => addFocus(gameLevelButton1, gameLevelButton2, gameLevelButton3));
+gameLevelButton2.addEventListener('click', () => addFocus(gameLevelButton2, gameLevelButton3, gameLevelButton1));
+gameLevelButton3.addEventListener('click', () => addFocus(gameLevelButton3, gameLevelButton2, gameLevelButton1));
 
-
-gameLevelButton1.addEventListener('click', () => {
-    gameLevelButton1.classList.add('focus');
-    gameLevelButton2.classList.remove('focus');
-    gameLevelButton3.classList.remove('focus');
+function addFocus(element1, element2, element3) {
+    element1.classList.add('focus'); 
+    element2.classList.remove('focus'); 
+    element3.classList.remove('focus'); 
 }
-)
-
-gameLevelButton2.addEventListener('click', () => {
-    gameLevelButton2.classList.add('focus');
-    gameLevelButton1.classList.remove('focus');
-    gameLevelButton3.classList.remove('focus');
-}
-)
-
-gameLevelButton3.addEventListener('click', () => {
-    gameLevelButton3.classList.add('focus');
-    gameLevelButton2.classList.remove('focus');
-    gameLevelButton1.classList.remove('focus');
-}
-)
 
 buttonStart.addEventListener('click', getLevelDifficulty)
 
